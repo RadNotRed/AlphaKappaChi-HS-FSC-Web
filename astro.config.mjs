f75e741a09c482948894798575624ac057a58ed3
RadNotRed <80015713+RadNotRed@users.noreply.github.com.>
2026-03-11 23:40:32 -0400

    test: allow showing on site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://RadNotRed.github.io',
+  base: '/AlphaKappaChi-HS-FSC-Web',
   integrations: [tailwind()],
 });
