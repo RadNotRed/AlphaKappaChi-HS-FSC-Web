4df0df17eaf707dcf967214d0ed78ded833bca9c
RadNotRed <80015713+RadNotRed@users.noreply.github.com.>
2026-03-11 23:44:30 -0400

    fix: actions dependant listing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://RadNotRed.github.io',
-  base: '/AlphaKappaChi-HS-FSC-Web',
+  base: process.env.GITHUB_ACTIONS ? '/AlphaKappaChi-HS-FSC-Web' : '/',
   integrations: [tailwind()],
 });
